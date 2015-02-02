a6c9444d4959369436fb87a14f8855587a49672a
Max <MPG1019@aol.com>
2015-02-01 18:13:19 -0600

    Initial release

---
+++ RobloxModManager/Launcher.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace RobloxModManager
+{
+    partial class Launcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Launcher));
+            this.launchStudio = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.manageMods = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // launchStudio
+            // 
+            this.launchStudio.Cursor = System.Windows.Forms.Cursors.Default;
+            this.launchStudio.Location = new System.Drawing.Point(78, 173);
+            this.launchStudio.Name = "launchStudio";
+            this.launchStudio.Size = new System.Drawing.Size(99, 23);
+            this.launchStudio.TabIndex = 6;
+            this.launchStudio.Text = "Launch Studio";
+            this.launchStudio.UseVisualStyleBackColor = true;
+            this.launchStudio.Click += new System.EventHandler(this.launchStudio_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(155, 142);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // manageMods
+            // 
+            this.manageMods.Cursor = System.Windows.Forms.Cursors.Default;
+            this.manageMods.Location = new System.Drawing.Point(78, 203);
+            this.manageMods.Name = "manageMods";
+            this.manageMods.Size = new System.Drawing.Size(99, 23);
+            this.manageMods.TabIndex = 9;
+            this.manageMods.Text = "Manage Mods";
+            this.manageMods.UseVisualStyleBackColor = true;
+            this.manageMods.Click += new System.EventHandler(this.manageMods_Click);
+            // 
+            // Launcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(255, 255);
+            this.Controls.Add(this.manageMods);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.launchStudio);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Launcher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Roblox Studio Mod";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button launchStudio;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button manageMods;
+    }
+}
+
---
+++ RobloxModManager/Launcher.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace RobloxModManager
+{
+    public partial class Launcher : Form
+    {
+        WebClient http = new WebClient();
+
+        public string getModPath()
+        {
+            string appData = Environment.GetEnvironmentVariable("AppData");
+            string root = Path.Combine(appData, "RbxModManager", "ModFiles");
+            if (!Directory.Exists(root))
+            {
+                // Build a folder structure so the usage of my mod manager is more clear.
+                string[] folderPaths = new string[]
+                {
+                    "BuiltInPlugins",
+                    "content/fonts",
+                    "content/music",
+                    "content/particles",
+                    "content/scripts",
+                    "content/sky",
+                    "content/sounds",
+                    "content/textures/loading",
+                    "content/textures/particles",
+                    "content/textures/ui",
+                    "PlatformContent/pc/textures/aluminum",
+                    "PlatformContent/pc/textures/brick",
+                    "PlatformContent/pc/textures/cobblestone",
+                    "PlatformContent/pc/textures/concrete",
+                    "PlatformContent/pc/textures/diamondplate",
+                    "PlatformContent/pc/textures/fabric",
+                    "PlatformContent/pc/textures/granite",
+                    "PlatformContent/pc/textures/grass",
+                    "PlatformContent/pc/textures/ice",
+                    "PlatformContent/pc/textures/marble",
+                    "PlatformContent/pc/textures/metal",
+                    "PlatformContent/pc/textures/pebble",
+                    "PlatformContent/pc/textures/plastic",
+                    "PlatformContent/pc/textures/rust",
+                    "PlatformContent/pc/textures/sand",
+                    "PlatformContent/pc/textures/sky",
+                    "PlatformContent/pc/textures/slate",
+                    "PlatformContent/pc/textures/terrain",
+                    "PlatformContent/pc/textures/water",
+                    "PlatformContent/pc/textures/wood",
+                    "PlatformContent/pc/textures/woodplanks"
+                };
+                foreach (string f in folderPaths)
+                {
+                    string path = Path.Combine(root,f.Replace("/", "\\"));
+                    Directory.CreateDirectory(path);
+                }
+            }
+            return root;
+        }
+
+        public void DownloadStudio(string path)
+        {
+            string filePath = Path.Combine(path, "RobloxStudioLauncherBeta.exe");
+            if (!File.Exists(filePath))
+            {
+                byte[] RobloxStudio = http.DownloadData("http://setup.roblox.com/RobloxStudioLauncherBeta.exe");
+                FileStream download = File.Create(filePath);
+                download.Write(RobloxStudio, 0, RobloxStudio.Length);
+                download.Close();
+            }
+            Process studio = Process.Start(filePath);
+            studio.WaitForExit();
+            Process[] running = Process.GetProcessesByName("RobloxStudioBeta");
+            foreach (Process p in running)
+            {
+                try
+                {
+                    p.Kill();
+                }
+                catch
+                {
+                    // Sometimes it won't let us :/
+                }
+            }
+        }
+
+        public string getStudioExePath()
+        {
+            string[] envPaths = new string[] { 
+                Environment.GetEnvironmentVariable("LocalAppData"), 
+                Environment.GetEnvironmentVariable("ProgramFiles")
+            };
+            string version = http.DownloadString("http://setup.roblox.com/versionQTStudio");
+            string exePath = null;
+            foreach (string envPath in envPaths)
+            {
+                string directory = Path.Combine(envPath, "Roblox", "Versions", version, "RobloxStudioBeta.exe");
+                if (File.Exists(directory))
+                {
+                    exePath = directory;
+                    break;
+                }
+            }
+            if (exePath != null)
+            {
+                return exePath;
+            }
+            else
+            {
+                string appData = Environment.GetEnvironmentVariable("AppData");
+                string root = Path.Combine(appData, "RbxModManager");
+                if (!Directory.Exists(root))
+                {
+                    Directory.CreateDirectory(root);
+                }
+                DownloadStudio(root);
+                foreach (string envPath in envPaths)
+                {
+                    string directory = Path.Combine(envPath, "Roblox", "Versions", version, "RobloxStudioBeta.exe");
+                    if (File.Exists(directory))
+                    {
+                        return directory;
+                    }
+                }
+            }
+            throw new Exception("Couldn't locate Roblox Studio");
+        }
+
+        public Launcher()
+        {
+            InitializeComponent();
+        }
+
+        private void manageMods_Click(object sender, EventArgs e)
+        {
+            string modPath = getModPath();
+            Process.Start(modPath);
+        }
+
+        private void launchStudio_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            // Check mod folders to see if we need to override anything, then launch.
+            string studioPath = getStudioExePath();
+            string studioRoot = Directory.GetParent(studioPath).ToString();
+            string modPath = getModPath();
+            string[] studioFiles = Directory.GetFiles(studioRoot);
+            string[] modFiles = Directory.GetFiles(modPath,"*.*",SearchOption.AllDirectories);
+            foreach (string modFile in modFiles)
+            {
+                try
+                {
+                    byte[] fileContents = File.ReadAllBytes(modFile);
+                    FileInfo modFileControl = new FileInfo(modFile);
+                    string relativeFile = modFile.Replace(modPath, studioRoot);
+                    byte[] studioFile = File.ReadAllBytes(relativeFile);
+                    if (!fileContents.SequenceEqual(studioFile))
+                    {
+                        modFileControl.CopyTo(relativeFile, true);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to overwrite " + modFile + "\nMight be open in another program\nThats their problem, not mine <3");
+                }
+            }
+            Process Studio = Process.Start(studioPath);
+            Studio.WaitForExit();
+            Application.Exit();
+        }
+    }
+}
